e29b49898e233c72086eb1cc2fcc7e941b4ee214
edwinhcw <edwinhcw@gmail.com>
2015-06-04 22:44:05 +0800

    stock updata

--- src/blacksmith/smithing/Stock.java
+++ src/blacksmith/smithing/Stock.java
@@ -1,13 +1,48 @@
 package blacksmith.smithing;
 
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Stock extends JPanel{
+public class Stock extends JPanel implements ActionListener{
+    
     private JLabel text;
+    private JPanel Sower;
+    private JButton Mineral;
+    private JButton Equipment;
+    private JButton Katana;
+    private JLabel Word;
+    
     public Stock(){
         text = new JLabel("the bear");
         this.add(text);
+        
+        
+        Mineral = new JButton("Mineral");
+        Equipment = new JButton("Equipment");
+        Katana = new JButton("Katana");
+        Word = new JLabel("Stock");
+        
+//        Mineral.setBounds(40,40,50,50);
+//        Equipment.setBounds(100,100,50,50);
+//        Katana.setBounds(150,150,40,40);
+        
+        Mineral.addActionListener(this);
+        Equipment.addActionListener(this);
+        Katana.addActionListener(this);
+        
+        add(Mineral);
+        add(Equipment);
+        add(Katana);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        
     }
 }
